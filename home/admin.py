174bbdba3ee255a84e0c5329080ed59b0e61cf71
HoangCoder <phambahuyhoang0@gmail.com>
2022-07-25 11:27:02 +0700

    version 2

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,16 @@
+from msilib.schema import Class
 from django.contrib import admin
-from .models import Landing
+from .models import HomeButton, HomeContent, Landing, Navigation, About, Achievement, Classroom, HomeButton, Emath
 # Register your models here.
 
 admin.site.register(Landing)
+admin.site.register(Navigation)
+admin.site.register(HomeContent)
+admin.site.register(HomeButton)
+admin.site.register(About)
+admin.site.register(Achievement)
+admin.site.register(Classroom)
+admin.site.register(Emath)
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from home.models import Landing
+from .models import Landing, Navigation, HomeContent, About, Achievement, Classroom, HomeButton, Emath
 
 # Create your views here.
 
@@ -10,7 +10,25 @@ class Home(TemplateView):
 
   def get_context_data(self, **kwargs):
     landing = Landing.objects.all().order_by('-priority').first()
+    navbar = Navigation.objects.all()
+    home_content = HomeContent.objects.all()
+    about = About.objects.all()
+    achievement = Achievement.objects.all()
+    classes = Classroom.objects.all()
+    buttons = HomeButton.objects.all()
+    emath = Emath.objects.all()
+
+    
+    
     context = super().get_context_data(**kwargs)
     context['description'] = landing.about
     context['title'] = landing.title
+    context['nav'] = navbar
+    context['home_info'] = home_content
+    context['buttons_section'] = buttons
+    context['about_section'] = about
+    context['achievement_section'] = achievement
+    context['classes_section'] = classes
+    context['emath_section'] = emath
+
     return context
