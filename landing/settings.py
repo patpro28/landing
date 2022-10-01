0345b75edd126ef97923cb6e2731feff314034cc
nguyenductoandhv <nguyenductoandhv@gmail.com>
2022-10-01 10:59:25 +0700

    fix js

--- landing/settings.py
+++ landing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--bwm&nlxvr3kp1w+c97&lc55lb#l0lh=rq)5@c%zl=&8pg+-f3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -208,6 +208,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
